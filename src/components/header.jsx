33b5a3880352123f77e498c05a2e7a7664ef44aa
megha-56 <meghapanthi435@gmail.com>
2026-03-16 22:45:45 +0530

    added basic header

---
+++ src/components/header.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+
+const Header=()=>{
+    return(
+        // </div>
+        <div
+          className="h-64 w-screen bg-cover bg-[center_-25px] flex items-center justify-center gap-4"
+         style={{ backgroundImage: `url("/logo.png")` }} >
+                                                
+        {/* <button className="bg-blue-500 text-white px-6 py-2 rounded-lg hover:bg-blue-600">
+           Order Now
+        </button>
+
+         <button className="bg-white text-black px-6 py-2 rounded-lg hover:bg-gray-200">
+           View Menu
+         </button> */}
+      </div>
+    )
+
+}
+export default Header
